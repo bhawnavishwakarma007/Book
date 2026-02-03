68c4f6151bfeb580405f6fc09a453e3fb3456c45
bhawna <bhawnavishwakarma464@gmail.com>
2026-02-03 22:44:36 +0530

    Book

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import cookieParser from "cookie-parser";
-import express from "express";
-import routes from "./routes/index.js";
-import { ConnectDB } from "./model/index.js";
-import { PORT } from "./config/env.js";
-
 const app = express();
+
+app.set("trust proxy", 1); // ✅ ADD THIS
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,8 +12,8 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.use("/api", routes);
-
-app.listen(PORT || 3000, () => {
-  console.log(`Server running at port ${PORT}`);
+app.get("/health", (req, res) => {   // ✅ ADD THIS
+  res.status(200).json({ status: "OK" });
 });
+
+app.use("/api", routes);
