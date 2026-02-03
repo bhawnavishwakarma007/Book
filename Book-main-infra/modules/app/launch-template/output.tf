38b76bcafd7bd9db70dc329256602f813fcd558b
Bhawna Vishwakarma <159766023+bhawnavishwakarma007@users.noreply.github.com>
2026-02-03 23:27:44 +0530

    Update output.tf

--- Book-main-infra/modules/app/launch-template/output.tf
+++ Book-main-infra/modules/app/launch-template/output.tf
@@ -1,11 +1,9 @@
-output "app_ami_id" {
-  description = "app AMI ID"
-  value       = aws_ami_from_instance.app_ami.id
+output "app_launch_template_id" {
+  value = aws_launch_template.app.id
 }
 
-output "app_launch_template_id" {
-  description = "app Launch Template ID"
-  value       = aws_launch_template.app.id
+output "app_ami_id" {
+  value = aws_ami_from_instance.app_ami.id
 }
 
 output "app_launch_template_latest_version" {
