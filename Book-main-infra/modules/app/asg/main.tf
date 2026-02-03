13793a518f05d34f1f722f40ed0ef533b46d475f
Bhawna Vishwakarma <159766023+bhawnavishwakarma007@users.noreply.github.com>
2026-02-03 23:28:47 +0530

    Update main.tf

--- Book-main-infra/modules/app/asg/main.tf
+++ Book-main-infra/modules/app/asg/main.tf
@@ -1,7 +1,3 @@
-##################################
-# Frontend Auto Scaling Group
-##################################
-
 resource "aws_autoscaling_group" "app_asg" {
   name             = var.app_asg_name
   min_size         = var.min_size
@@ -9,42 +5,24 @@ resource "aws_autoscaling_group" "app_asg" {
   desired_capacity = var.desired_capacity
 
   target_group_arns = [var.target_group_arn]
+  vpc_zone_identifier = var.private_subnet_ids
 
   launch_template {
     id      = var.launch_template_id
     version = "$Latest"
   }
 
-  vpc_zone_identifier = var.private_subnet_ids
-
   instance_refresh {
     strategy = "Rolling"
     preferences {
       min_healthy_percentage = 50
     }
-    triggers = ["desired_capacity"]
+    triggers = ["launch_template"]
   }
 
   tag {
     key                 = "Name"
     value               = var.app_asg_name
     propagate_at_launch = true
-  }
-}
-
-##################################
-# Frontend Scale-Out Policy
-##################################
-
-resource "aws_autoscaling_policy" "app_scale_out" {
-  name                   = var.app_scale_out_policy_name
-  autoscaling_group_name = aws_autoscaling_group.app_asg.name
-  policy_type            = "TargetTrackingScaling"
-
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
-    target_value = var.scale_out_target_value
   }
 }
