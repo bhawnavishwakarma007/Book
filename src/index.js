1a523bd94a301d209f5c5fd6e1c8e715d088422b
Bhawna Vishwakarma <159766023+bhawnavishwakarma007@users.noreply.github.com>
2026-02-04 18:00:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import routes from "./routes/index.js";
+import { ConnectDB } from "./model/index.js";
+import { PORT } from "./config/env.js";
+
 const app = express();
 
-app.set("trust proxy", 1); // ✅ ADD THIS
+app.set("trust proxy", 1);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -12,8 +18,12 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.get("/health", (req, res) => {   // ✅ ADD THIS
+app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK" });
 });
 
 app.use("/api", routes);
+
+app.listen(PORT || 3000, () => {
+  console.log(`Server running at port ${PORT}`);
+});
